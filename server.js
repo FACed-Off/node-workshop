53e3034f9d34034c36335cf7a1b2e8fc702b3040
Meeran B <v9@MacBook-Air.local>
2020-06-23 10:17:35 +0100

    add handler function for requests

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const http = require("http");
 
-const server = http.createServer();
+const message = 'I am so happy to be part of the Node Girls workshop!'
+
+function handler(request, response) {
+    response.writeHead(200, {"content-type":"text/html"});
+    response.write(message);
+    response.end();
+}
+
+const server = http.createServer(
+    handler
+);
 
 server.listen(3000, () => console.log("Server is listening on port 3000. Ready to accept requests."))
