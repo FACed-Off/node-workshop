9798ea98c23f5fe8d6e76c718df3df2ad20a5c4f
Meeran B <v9@MacBook-Air.local>
2020-06-23 13:05:58 +0100

    begun POST method support

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const fs = require("fs");
+const querystring = require("querystring");
 
 const message = 'I am so happy to be part of the Node Girls workshop!'
 
@@ -9,9 +10,9 @@ function handler(request, response) {
     console.log(`${method} ${endpoint}`);
 
     if (endpoint === "/") {
-        response.writeHead(200, {"content-type":"text/html"});
+        response.writeHead(200, { "content-type": "text/html" });
 
-        fs.readFile(__dirname + "/public/index.html", function(error, file){
+        fs.readFile(__dirname + "/public/index.html", function (error, file) {
             if (error) {
                 console.log(error);
                 return;
@@ -20,19 +21,32 @@ function handler(request, response) {
         });
 
     } else if (endpoint === "/girls") {
-        response.writeHead(200, {"content-type":"text/html"})
+        response.writeHead(200, { "content-type": "text/html" })
         response.write("girls url reached")
         response.end();
     } else if (endpoint === "/node") {
-        response.writeHead(200, {"content-type":"text/html"})
+        response.writeHead(200, { "content-type": "text/html" })
         response.write("node url reached")
         response.end();
     } else {
         const fileExtension = endpoint.split(".")[1]
-        response.writeHead(200, {"content-type":`text/${fileExtension}`})
+        response.writeHead(200, { "content-type": `text/${fileExtension}` })
 
-        fs.readFile(__dirname + `/public${endpoint}`, function(error, file){
+        fs.readFile(__dirname + `/public${endpoint}`, function (error, file) {
+
             console.log(`sending ${endpoint}`)
+
+            let allTheData = '';
+            request.on('data', function (chunkOfData) {
+                allTheData += chunkOfData;
+            })
+
+            request.on('end', function () {
+                const convertedData = querystring.parse(allTheData)
+                console.log(convertedData);
+                response.end();
+            })
+
             if (error) {
                 console.log(error);
                 return;
@@ -41,6 +55,7 @@ function handler(request, response) {
         })
     }
 }
+
 
 const server = http.createServer(
     handler
