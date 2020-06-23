c6af4f772e35b7534146036e9b393b36b2a21dbf
Meeran B <v9@MacBook-Air.local>
2020-06-23 10:40:48 +0100

    link index.html in handler

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const message = 'I am so happy to be part of the Node Girls workshop!'
 
@@ -7,7 +8,18 @@ function handler(request, response) {
     const method = request.method;
     console.log(`${method} ${endpoint}`);
 
-    if (endpoint === "/girls") {
+    if (endpoint === "/") {
+        response.writeHead(200, {"content-type":"text/html"});
+
+        fs.readFile(__dirname + "/public/index.html", function(error, file){
+            if (error) {
+                console.log(error);
+                return;
+            }
+            response.end(file)
+        });
+
+    } else if (endpoint === "/girls") {
         response.writeHead(200, {"content-type":"text/html"})
         response.write("girls url reached")
         response.end();
