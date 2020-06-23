02d7c3290668c06daefc286983c7ebcb9ac04415
Meeran B <v9@MacBook-Air.local>
2020-06-23 10:32:36 +0100

    request endpoint handling added

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const http = require("http");
 const message = 'I am so happy to be part of the Node Girls workshop!'
 
 function handler(request, response) {
-    response.writeHead(200, {"content-type":"text/html"});
-    response.write(message);
-    response.end();
+    const endpoint = request.url;
+    const method = request.method;
+    console.log(`${method} ${endpoint}`);
+
+    if (endpoint === "/girls") {
+        response.writeHead(200, {"content-type":"text/html"})
+        response.write("girls url reached")
+        response.end();
+    } else if (endpoint === "/node") {
+        response.writeHead(200, {"content-type":"text/html"})
+        response.write("node url reached")
+        response.end();
+    }
 }
 
 const server = http.createServer(
