ebcedd750525a320791f76e352622fe7e4a05a33
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-23 14:28:35 +0100

    abstracted handler function

--- server.js
+++ server.js
@@ -1,67 +1,8 @@
 const http = require("http");
-const fs = require("fs");
-const querystring = require("querystring");
+const handler = require("./src/handlers");
 
-const message = 'I am so happy to be part of the Node Girls workshop!'
+const server = http.createServer(handler);
 
-function handler(request, response) {
-    const endpoint = request.url;
-    const method = request.method;
-    console.log(`${method} ${endpoint}`);
-
-    if (endpoint === "/") {
-        response.writeHead(200, { "content-type": "text/html" });
-
-        fs.readFile(__dirname + "/public/index.html", function (error, file) {
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file)
-        });
-
-    } else if (endpoint === "/girls") {
-        response.writeHead(200, { "content-type": "text/html" })
-        response.write("girls url reached")
-        response.end();
-    } else if (endpoint === "/node") {
-        response.writeHead(200, { "content-type": "text/html" })
-        response.write("node url reached")
-        response.end();
-    } else if (endpoint === "/create-post") {
-        response.writeHead(302, {"location": "/"})
-        response.end();
-    } else {
-        const fileExtension = endpoint.split(".")[1]
-        response.writeHead(200, { "content-type": `text/${fileExtension}` })
-
-        fs.readFile(__dirname + `/public${endpoint}`, function (error, file) {
-
-            console.log(`sending ${endpoint}`)
-
-            let allTheData = '';
-            request.on('data', function (chunkOfData) {
-                allTheData += chunkOfData;
-            })
-
-            request.on('end', function () {
-                const convertedData = querystring.parse(allTheData)
-                console.log(convertedData);
-                response.end();
-            })
-
-            if (error) {
-                console.log(error);
-                return;
-            }
-            response.end(file);
-        })
-    }
-}
-
-
-const server = http.createServer(
-    handler
+server.listen(3000, () =>
+  console.log("Server is listening on port 3000. Ready to accept requests.")
 );
-
-server.listen(3000, () => console.log("Server is listening on port 3000. Ready to accept requests."))
