ec533dc8bf922423194704bb72acadc47eb4b883
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-06-29 20:20:08 +0100

    feat: create new server

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const handler = require("./src/handlers");
+
+const server = http.createServer(handler);
+
+server.listen(3000, () =>
+  console.log("Server is listening on port 3000. Ready to accept requests.")
+);
