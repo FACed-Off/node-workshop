db37050aad30a4f7c698ce2fcb05b5b6bfff4345
Minesh Shah <mineshmshah@hotmail.co.uk>
2020-07-07 15:06:09 +0100

    added test for no index file

--- src/handlers/home.js
+++ src/handlers/home.js
@@ -2,10 +2,16 @@ const fs = require("fs");
 
 function homeHandler(request, response) {
   response.writeHead(200, { "content-type": "text/html" });
+  console.log("hello1");
 
   fs.readFile(__dirname + "/../../public/index.html", function (error, file) {
+    /* istanbul ignore else */
     if (error) {
-      return;
+      console.log("hello");
+      response.writeHead(500, {
+        "content-type": "text/html",
+      });
+      response.end("<h1>Server Error</h1>");
     }
     response.end(file);
   });
--- tests/home.test.js
+++ tests/home.test.js
@@ -1,27 +1,29 @@
 const supertest = require("supertest");
 const router = require("../router");
-const home = require("../src/handlers/home");
+const mock = require("mock-fs");
 
-// describe("Sample Test", () => {
-//   it("should test that true === true", () => {
-//     expect(true).toBe(true);
-//   });
-// });
+const response = jest.fn();
 
-test("Home route returns a status code of 200", () => {
-  //console.log(supertest(router).get("/"));
+describe("testing with no file supplied", () => {
+  beforeEach(() => {
+    mock();
+  });
+  it("returns a status code of 500 when no index file exists", () => {
+    return supertest(router)
+      .get("/")
+      .expect(500)
+      .expect("content-type", "text/html");
+  });
+
+  afterEach(() => {
+    mock.restore();
+  });
+});
+
+it("returns a status code of 200 on the home root", () => {
   return supertest(router)
     .get("/")
     .expect(200)
     .expect("content-type", "text/html")
-    .end((err, res) => {
-      if (err) throw err;
-    });
+    .then((response) => expect(response.status).toBe(200));
 });
-
-// test("readFile method returns error upon failure", () => {
-//   return supertest(home)
-//     .get("")
-//     .expect(404)
-//     .expect("content-type", "text/html");
-// });
